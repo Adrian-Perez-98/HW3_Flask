f95fb21f7d9e4e34476e0aeb80707b6a1ea99cf8
Adrian Perez <aperez@ehnusa.com>
2021-04-26 13:52:52 -0700

    Start

---
+++ .history/app/models_20210420221717.py
@@ -0,0 +1,26 @@
+from app import db
+
+class User(db.Model):
+    # have the following columns
+    # id (int)
+    id = db.Column(db.Integer, primary_key=True)
+    # author (string, unique, can't be null)
+    author = db.Column(db.String, nullable=False, unique=True)
+    # message (linkd to Messages table)
+    message = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<User {self.author}>'
+
+class Messages(db.Model):
+    # have the following columns
+    # id (int)
+    id = db.Column(db.Integer, primary_key=True)
+    # message (string, not unique, can't be null)
+    # user_id link to id (int)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # write __repr__ that outputs
+    def __repr__(self):
+        return '<Message {message}>'.format(self.body)
+    # <Message: MESSAGE_GOES_HERE>
+    # replace MESSAGE_GOES_HERE with the message
